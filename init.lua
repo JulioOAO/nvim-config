3940ecbd90fd4a47a9bb8ba3bf36969157fb5ebd
Julio Arias <juliooao@gmail.com>
2025-08-21 12:48:31 -0600

    Initial commit

---
+++ init.lua
@@ -0,0 +1,10 @@
+-- Set <space> as the leader key
+-- See `:help mapleader`
+--  NOTE: Must happen before plugins are loaded (otherwise wrong leader will be used)
+vim.g.mapleader = ' '
+vim.g.maplocalleader = ' '
+
+require('config.lazy') -- Load before 'config.options' because catpuccin is called in there
+require('config.options')
+require('config.keymaps')
+require('config.autocmds')
---
+++ lua/config/keymaps.lua
@@ -0,0 +1,19 @@
+local map = vim.keymap.set
+local opts = { noremap = true, silent = true }
+
+-- Keybinds to make split navigation easier.
+--  Use CTRL+<hjkl> to switch between windows
+map('n', '<C-h>', '<C-w><C-h>', { desc = 'Move focus to the left window' })
+map('n', '<C-l>', '<C-w><C-l>', { desc = 'Move focus to the right window' })
+map('n', '<C-j>', '<C-w><C-j>', { desc = 'Move focus to the lower window' })
+map('n', '<C-k>', '<C-w><C-k>', { desc = 'Move focus to the upper window' })
+
+-- Clear highlights on search when pressing <Esc> in normal mode
+map('n', '<Esc>', '<cmd>nohlsearch<CR>')
+
+map('t', '<Esc>', '<C-\\><C-n>', { desc = '[T]erminal: Exit insert mode' })
+
+map('n', '<leader>e', '<cmd>Neotree filesystem reveal toggle<CR>', { desc = 'Toggle Neotree' })
+
+-- Diagnostic keymaps
+vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagnostic [Q]uickfix list' })
