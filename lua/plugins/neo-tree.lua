f0ce81896d23191eafe5a6d499f15bd1f52c9ed7
Julio Arias <juliooao@gmail.com>
2025-08-26 10:27:55 -0600

    Testing neo-tree filesystem commands

--- lua/plugins/neo-tree.lua
+++ lua/plugins/neo-tree.lua
@@ -7,6 +7,10 @@ return {
             "MunifTanjim/nui.nvim",
             "nvim-tree/nvim-web-devicons",
         },
+        config = function()
+            require("neo-tree.sources.filesystem.commands").refresh(require("neo-tree.sources.manager").get_state(
+            "filesystem"))
+        end
     },
     {
         "antosha417/nvim-lsp-file-operations",
